b21e6a8d38293926367ada70f5fa1122b9620b70
Saksham444444 <saksham44humagain@gmail.com>
2024-01-15 20:54:13 +0545

    Day-2

--- question.c
+++ question.c
@@ -75,4 +75,83 @@ int main(){
     printf("%d",ch);
 return 0;
 }
-// changes has been added
+//5... To swap the two numbrers
+int main(){
+    int a,b;
+    printf("Enter two numbers a and b\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("a:%d and b:%d\n",a,b);
+    // if a=4 b=5 a=4+5=9
+    a= a+b;
+    // b=9-5 so b is 4
+    b=a-b;
+    // a=9-4 so a is 5
+    a=a-b;
+    printf("a: %d and b: %d",a,b);
+}
+//6.. To find the size of
+#include <stdio.h>
+int main(){
+    int a = 5;
+    float b = 5.34;
+    char c = 'f';
+    double e= 4.5;
+    printf("%d\n",sizeof(a));
+     printf("%d\n",sizeof(b));
+      printf("%d\n",sizeof(c));
+       printf("%d\n",sizeof(e));
+
+
+}//7... To find the prime numbers from 1 to n
+#include <stdio.h>
+int main(){
+    int n,i,j;
+    printf("Enter the numbers of terms\n");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+         int prime=1;
+         if(i==1){
+            prime=0;
+         }
+        for(j=2;j<=i/2;j++){
+            if(i %j == 0){
+            prime=0;
+            break;
+            }
+            
+        }
+        if(prime==1){
+            printf("%d,",i);
+        }
+        }
+
+    }
+//8... Program to find the simple intrest
+#include <stdio.h>
+int main(){
+    int p,t;
+    float r;
+    printf("Enter the principle");
+    scanf("%d",&p);
+    printf("Enter the rate");
+    scanf("%f", &r);
+    printf("Enter the time period");
+    scanf("%d",&t);
+     r = r/100;
+    float amount = p*(1+t*r);
+    float intrest = amount - p;
+    printf("Amount= %.2f and intrest=%.2f",amount,intrest);
+    return 0;
+}//To print your own name
+#include <stdio.h>
+int main(){
+ char name[345];
+ printf("Enter your name\n");
+ //It ignore the tes=xt that is after the space
+ scanf("%s",name);
+ // It helps to write a complete sentence using array and include spaces and tab
+ scanf(" %[^\n]", name );
+ printf("My name is %s",name);
+ return 0;
+}
