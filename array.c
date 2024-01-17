4c47ae8b8f4395ff3d4d45aa33238b3f6b864fe1
Saksham444444 <saksham44humagain@gmail.com>
2024-01-17 21:01:47 +0545

    day4.1

---
+++ array.c
@@ -0,0 +1,12 @@
+//ARRAY
+//Array in C is a fixed size collection of similar data items stored in contigious memory location
+//Array in C is one of the most used data structures in C programming. 
+//It is a simple and fast way of storing multiple values under a single name
+//It can be used to store user define data type such as pointers and structure
+// Syntax
+data_type name_name[size1] [size2] [sizeN];
+// here N is the number of dimension
+#include <stdio.h>
+//Array declearion
+int neppal[34];
+char ch[34];
