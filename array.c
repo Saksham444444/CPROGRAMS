128cb9997950474530f7ecde1e36e7b81e5c8a6f
Saksham444444 <saksham44humagain@gmail.com>
2024-01-18 21:01:43 +0545

    day5.1

--- array.c
+++ array.c
@@ -83,3 +83,19 @@ int main(){
         printf("%s",ch);
 
 }
+//2d array 
+#include <stdio.h>
+int main(){
+    int i,j;
+    int number[3][3] = {1,2,3,4,5,6,7,8,9};
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+        printf("(%d,%d)%d\t",i,j,number[i][j]);
+        }
+        printf("\n");
+    }
+}
+//Creating a 2d array
+int matrix[3][4] ={{2,4,3,4},{2,4,3,2},{4,7,8,6}};
+//The first dimension represents the number of rows 
+//The second dimension represents the numbers of column
--- program.c
+++ program.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-    int main(){
-        char ch[65] = {'c','r','t'};
-        printf("%c",ch[1]);
-   
-    
+int main(){
+    int i,j;
+    int number[3][3] = {1,2,3,4,5,6,7,8,9};
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+        printf("(%d,%d)%d\t",i,j,number[i][j]);
+        }
+        printf("\n");
+    }
 }
